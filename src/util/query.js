f9419f5799a6a54833532b07983e026fc42e2fb0
Tomxuetao <1127513120@qq.com>
2020-01-06 13:50:45 +0800

    格式化代码

--- src/util/query.js
+++ src/util/query.js
@@ -10,86 +10,86 @@ const commaRE = /%2C/g
 // - escapes [!'()*]
 // - preserve commas
 const encode = str => encodeURIComponent(str)
-  .replace(encodeReserveRE, encodeReserveReplacer)
-  .replace(commaRE, ',')
+    .replace(encodeReserveRE, encodeReserveReplacer)
+    .replace(commaRE, ',')
 
 const decode = decodeURIComponent
 
 export function resolveQuery (
-  query: ?string,
-  extraQuery: Dictionary<string> = {},
-  _parseQuery: ?Function
+    query: ?string,
+    extraQuery: Dictionary<string> = {},
+    _parseQuery: ?Function
 ): Dictionary<string> {
-  const parse = _parseQuery || parseQuery
-  let parsedQuery
-  try {
-    parsedQuery = parse(query || '')
-  } catch (e) {
-    process.env.NODE_ENV !== 'production' && warn(false, e.message)
-    parsedQuery = {}
-  }
-  for (const key in extraQuery) {
-    parsedQuery[key] = extraQuery[key]
-  }
-  return parsedQuery
+    const parse = _parseQuery || parseQuery
+    let parsedQuery
+    try {
+        parsedQuery = parse(query || '')
+    } catch (e) {
+        process.env.NODE_ENV !== 'production' && warn(false, e.message)
+        parsedQuery = {}
+    }
+    for (const key in extraQuery) {
+        parsedQuery[key] = extraQuery[key]
+    }
+    return parsedQuery
 }
 
 function parseQuery (query: string): Dictionary<string> {
-  const res = {}
-
-  query = query.trim().replace(/^(\?|#|&)/, '')
-
-  if (!query) {
-    return res
-  }
-
-  query.split('&').forEach(param => {
-    const parts = param.replace(/\+/g, ' ').split('=')
-    const key = decode(parts.shift())
-    const val = parts.length > 0
-      ? decode(parts.join('='))
-      : null
-
-    if (res[key] === undefined) {
-      res[key] = val
-    } else if (Array.isArray(res[key])) {
-      res[key].push(val)
-    } else {
-      res[key] = [res[key], val]
+    const res = {}
+    
+    query = query.trim().replace(/^(\?|#|&)/, '')
+    
+    if (!query) {
+        return res
     }
-  })
-
-  return res
+    
+    query.split('&').forEach(param => {
+        const parts = param.replace(/\+/g, ' ').split('=')
+        const key = decode(parts.shift())
+        const val = parts.length > 0
+            ? decode(parts.join('='))
+            : null
+        
+        if (res[key] === undefined) {
+            res[key] = val
+        } else if (Array.isArray(res[key])) {
+            res[key].push(val)
+        } else {
+            res[key] = [res[key], val]
+        }
+    })
+    
+    return res
 }
 
 export function stringifyQuery (obj: Dictionary<string>): string {
-  const res = obj ? Object.keys(obj).map(key => {
-    const val = obj[key]
-
-    if (val === undefined) {
-      return ''
-    }
-
-    if (val === null) {
-      return encode(key)
-    }
-
-    if (Array.isArray(val)) {
-      const result = []
-      val.forEach(val2 => {
-        if (val2 === undefined) {
-          return
+    const res = obj ? Object.keys(obj).map(key => {
+        const val = obj[key]
+        
+        if (val === undefined) {
+            return ''
         }
-        if (val2 === null) {
-          result.push(encode(key))
-        } else {
-          result.push(encode(key) + '=' + encode(val2))
+        
+        if (val === null) {
+            return encode(key)
         }
-      })
-      return result.join('&')
-    }
-
-    return encode(key) + '=' + encode(val)
-  }).filter(x => x.length > 0).join('&') : null
-  return res ? `?${res}` : ''
+        
+        if (Array.isArray(val)) {
+            const result = []
+            val.forEach(val2 => {
+                if (val2 === undefined) {
+                    return
+                }
+                if (val2 === null) {
+                    result.push(encode(key))
+                } else {
+                    result.push(encode(key) + '=' + encode(val2))
+                }
+            })
+            return result.join('&')
+        }
+        
+        return encode(key) + '=' + encode(val)
+    }).filter(x => x.length > 0).join('&') : null
+    return res ? `?${res}` : ''
 }
